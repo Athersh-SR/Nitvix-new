dc57fb0b325fdf442a5321e557313ab41c1c793a
Athersh-SR <athersh946@gmail.com>
2025-03-28 13:02:41 +0530

    Final commit

---
+++ Scripts/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(() => {
+        document.getElementById("preloader").style.display = "none"; 
+        document.getElementById("content").style.display = "block";
+    }, 1250); 
+};
